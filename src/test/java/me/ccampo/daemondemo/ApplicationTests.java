f1b7e222b9ba4f7103f00d9095421eacf0981fbd
Jon Barber <jon.barber@acm.org>
2017-04-30 22:18:06 +0100

    Bumped to gradle 2.9 & Spring Boot 1.5.3

--- src/test/java/me/ccampo/daemondemo/ApplicationTests.java
+++ src/test/java/me/ccampo/daemondemo/ApplicationTests.java
@@ -2,28 +2,29 @@ package me.ccampo.daemondemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.client.RestTemplate;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(Application.class)
-@WebIntegrationTest(randomPort = true)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
 
-    private final RestTemplate restTemplate = new TestRestTemplate();
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Value("${local.server.port}")
     private int port;
 
     @Test
     public void testHelloWorld() {
-        final String response = restTemplate.getForObject("http://localhost:" + port, String.class);
-        assertEquals("Hello world!", response);
+        final String body = restTemplate.getForObject("/", String.class);
+        assertThat(body).isEqualTo("Hello world!");
     }
 }
