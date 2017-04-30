0f812b5a031fb8b73f80f8df8f247b7150748ade
Jon Barber <jon.barber@acm.org>
2017-04-30 20:25:31 +0100

    Created systemd service

--- scripts/rpm/postInstall.sh
+++ scripts/rpm/postInstall.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
 chown daemon-demo:daemon-demo /opt/local/daemon-demo/log
+
+cat <<- EOF > /etc/systemd/system/daemon-demo.service
+    [Unit]
+    Description=Spring boot daemon demo
+    After=syslog.target
+
+    [Service]
+    User=daemon-demo
+    ExecStart=/opt/local/daemon-demo/bin/daemon-demo.jar
+    SuccessExitStatus=143
+
+    [Install]
+    WantedBy=multi-user.target
+EOF
+
+systemctl enable daemon-demo.service
+systemctl start daemon-demo.service
